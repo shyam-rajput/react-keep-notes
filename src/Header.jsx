d4ad96513ea76c8a54024a9f779d4f9b36f02ee2
Shyam <ssrshyam59@gmail.com>
2023-10-16 09:30:11 +0530

    This is my vscode change

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     <>
       <div className="header">
         <h2 className="h2">Keep Notes</h2>
+        <h3>This is my upcoming change from vscode</h3>
       </div>
     </>
   );
